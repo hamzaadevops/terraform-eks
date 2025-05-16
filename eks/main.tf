fb31859c6223c6d28bd8f65ce406aba8dee188d2
hamzaadevops <hamzi3307@gmail.com>
2025-05-16 16:15:12 +0500

    Update EKS module version and enable self-managed addons

--- eks/main.tf
+++ eks/main.tf
@@ -1,20 +1,29 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 20.31"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  bootstrap_self_managed_addons = false
-  cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
-  }
+  bootstrap_self_managed_addons = true
 
   # Optional: Enable the cluster endpoint public access
   cluster_endpoint_public_access = true
+#   access_entries = {
+#     example = {
+#       principal_arn = "arn:aws:iam::123456789012:user/my-user"
+
+#       policy_associations = {
+#         example = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+#           access_scope = {
+#             namespaces = ["default"]
+#             type       = "namespace"
+#           }
+#         }
+#       }
+#     }
+#   }
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
