4e3b17f2a6c27887367ba47897248e8625b0f6ff
hamzaadevops <hamzi3307@gmail.com>
2025-05-28 11:43:58 +0500

    feat(iam): Implement IAM role and policies for EKS access and create user membership

--- iam/main.tf
+++ iam/main.tf
@@ -1,7 +1,61 @@
-# module "irsa_oidc" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-eks-oidc-provider"
-#   version = "5.30.0"
+# Fetch your current AWS account ID
+data "aws_caller_identity" "current" {}
 
-#   cluster_name = module.eks.cluster_name
-#   oidc_provider_arn = module.eks.oidc_provider_arn
-# }
+# 1. The role that you want people to assume
+resource "aws_iam_role" "eks_access_to_iam" {
+  name = "eks_access_to_iam"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          # Trust your whole account—actual gatekeeping is in the group policy below
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# 2. Create the Administrators group
+resource "aws_iam_group" "administrators" {
+  name = "Administrators"
+}
+
+# 3. Give that group permission to assume the role
+#    So only members of Administrators can actually call sts:AssumeRole on eks_access_to_iam
+resource "aws_iam_group_policy" "allow_assume_eks_access" {
+  name  = "AllowAssumeEksAccess"
+  group = aws_iam_group.administrators.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Resource = aws_iam_role.eks_access_to_iam.arn
+      }
+    ]
+  })
+}
+
+#Create a user and add this in a iam group
+resource "aws_iam_user" "alice" {
+  name = "alice"
+}
+
+resource "aws_iam_user_group_membership" "alice_admin" {
+  user = aws_iam_user.alice.name
+  groups = [
+    aws_iam_group.administrators.name
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_access_to_iam.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
--- main.tf
+++ main.tf
@@ -13,3 +13,7 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 }
+
+module "iam" {
+  source = "./iam"
+}
